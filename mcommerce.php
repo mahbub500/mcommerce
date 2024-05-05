94a4246107d29c3363af0d13f5f9954f0633112d
mahbub500 <mahbubmr500@gmail.com>
2024-05-05 19:51:19 +0600

    Init MCommerce

---
+++ mcommerce.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Plugin Name: MCommerce
+ * Plugin URI: mahbub.com
+ * Description: A wandarfull ecommerce system
+ * Version: 0.09
+ * Author: Mahbub
+ * Author URI: mahbub.com
+ * Text Domain: mcommerce
+ * Domain Path: /i18n/languages/
+ * Requires at least: 6.3
+ * Requires PHP: 7.4
+ *
+ * @package MCommerce
+ */
+defined( 'ABSPATH' ) || exit;
