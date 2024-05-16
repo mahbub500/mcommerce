a49fc75da5c184415ae95b347fa21bf13781e91d
mahbub500 <mahbubmr500@gmail.com>
2024-05-16 23:37:56 +0600

    Product View complete

--- views/shortcodes/products.php
+++ views/shortcodes/products.php
@@ -24,21 +24,22 @@ $procuts =  Helper::get_posts( [
 ] );
 ?>
 <div class="container-fluid">
-    <div class="row">        
+    <div class="row"> 
+       
         <?php  foreach ( $procuts as  $id => $title ) { 
 
-        $product_data 		= new Prodcuts_data( $id );
-        $permalink   		= get_permalink( $id );
+        $product_data 	    = new Prodcuts_data( $id );
+        $permalink   	    = get_permalink( $id );
         $product_content    = $product_data->product->post_content ;
-        // Helper::pri( $product_data->product );
+        $post_image         = get_the_post_thumbnail_url( $product_data->post ) ;
             
         ?>
-        <div class="col-md-6">
-            <div class="card" style="width:400px">
-                <img class="card-img-top" src="img_avatar1.png" alt="Card image">
+        <div class="col-md-6">   
+            <div class="card ml-2" style="width:400px ">
+                <img class="card-img-top img-thumbnail rounded" style=" height: 300px; " src=" <?php echo $post_image ?> " alt="Product Image image">
                 <div class="card-body">
                     <h2 class="card-title"><a href="<?php esc_attr_e( $permalink ); ?>"><?php esc_html_e( $title ) ?></a></h2>
-                    <p class="card-text"> <?php echo $product_content ?> </p>
+                    <p class="card-text">Details :  <?php echo $product_content ?> </p>
                     <p>Price : <?php echo $product_data->get( 'mc_product_price' ) ?> </p>
                     <p>Quantity Remain : <?php echo $product_data->get( 'mc_product_quantity' ) ?> </p>
                     
@@ -46,9 +47,9 @@ $procuts =  Helper::get_posts( [
                 </div>
             </div>
             
-            </div>
-            
+            </div>             
         <?php } ?>
+        
         
     </div>
 </div>
